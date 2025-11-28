33d67d574bcd5c6de415fd070778fbeee450af86
FodorLevente <fodorl@kkszki.hu>
2025-11-28 09:12:17 +0100

    Menü elkészítése

--- Program.cs
+++ Program.cs
@@ -10,6 +10,47 @@ namespace NevekSzurese
     {
         static void Main(string[] args)
         {
+            while (true) {
+                Console.Clear();
+                Console.WriteLine("Kilépés");
+                Console.WriteLine("1 Névelji szóközök levágva");
+                Console.WriteLine("2 Csak nagybetűvel kezdődők");
+                Console.WriteLine("3 Csak számjegy nélküliek");
+                Console.WriteLine("4 Csak speciális jel nélküliek");
+                Console.WriteLine("5 Legalább egy szóközt tartalmazók");
+                Console.WriteLine("6 Csak rendes nevek");
+
+                string valaszt = Console.ReadLine();
+                if (valaszt == "0")
+                {
+                    return;
+                }
+                else if (valaszt == "1")
+                {
+
+                }
+                else if (valaszt == "2")
+                {
+
+                }
+                else if (valaszt == "3")
+                {
+
+                }
+                else if (valaszt == "4")
+                {
+
+                }
+                else if (valaszt == "5")
+                {
+
+                }
+                else if (valaszt == "6")
+                {
+
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
